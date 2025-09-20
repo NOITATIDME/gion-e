2c097a5d5579f1e03c36487e0a3a9880970d8b8c
NOITATIDME <chgt186@naver.com>
2025-09-21 00:09:58 +0900

    app.js 삭제, 라이브러리, env 추가

---
+++ app.config.js
@@ -0,0 +1,35 @@
+export default ({ config }) => {
+  return {
+    ...config,
+    name: "gion-e",
+    slug: "gion-e",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    extra: {
+      weatherApiUrl: process.env.WEATHER_API_URL,
+      weatherApiKey: process.env.WEATHER_API_KEY
+    },
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    }
+  };
+};
